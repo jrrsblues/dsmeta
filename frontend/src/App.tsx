19cdb3138ab57a6bcfee41a17d329d08d1a22209
jrrsblues <jrrsblues@gmail.com>
2022-09-06 17:04:08 -0300

    primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/NotificationButton";
 
 function App() {
-  return(<h1>Salve o Corinthians!</h1>)
+  return (
+    <>
+      <h1>Salve o Corinthians!</h1>
+      <NotificationButton />
+    </>
+  );
 }
 
-export default App
+export default App;
